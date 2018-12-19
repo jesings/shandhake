d8726c315cfd1a939b057400d556d06da4436209
jesings <jsinger@stuy.edu>
2018-12-18 19:39:44 -0500

    fifos don't work on WSL ANGERY ANGERY ANGERY ANGERY ANGERY

--- pipe_networking.c
+++ pipe_networking.c
@@ -11,7 +11,19 @@
   returns the file descriptor for the upstream pipe.
   =========================*/
 int server_handshake(int *to_client) {
-  return 0;
+    mkfifo("Gandalf",0644);
+    puts(strerror(errno));
+    puts("Server establishing connection to client");
+    *to_client = open("Gandalf",O_RDONLY);
+    char* pipename;
+    read(*to_client,pipename,HANDSHAKE_BUFFER_SIZE);
+    puts("Server recieved private pipe name");
+    int upstream = open(pipename,O_WRONLY);
+    puts("Connection established");
+    char* ack = ACK;
+    puts("Acknowledging having recieved the connection");
+    write(upstream,ACK,HANDSHAKE_BUFFER_SIZE);
+    return upstream;
 }
 
 
@@ -25,5 +37,16 @@ int server_handshake(int *to_client) {
   returns the file descriptor for the downstream pipe.
   =========================*/
 int client_handshake(int *to_server) {
-  return 0;
+    mkfifo("ipepay",0644);
+    int wrfd = open("Gandalf",O_WRONLY);
+    puts("Connection established");
+    char* ackbuff = "ipepay";
+    puts("Client giving private pipe name to server");
+    write(wrfd,ackbuff,HANDSHAKE_BUFFER_SIZE);
+    puts("Client establishing a connection with server");
+    *to_server =  open("ipepay",O_RDONLY);
+    char* ackwait;
+    read(*to_server,ackwait,HANDSHAKE_BUFFER_SIZE);
+    puts("Acknowledgement recieved");
+    return 0;
 }
